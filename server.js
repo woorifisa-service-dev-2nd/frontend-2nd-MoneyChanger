ecbc32d55e7aa53bfac626790cb9d62bdcf5ed02
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-11-30 12:58:55 +0900

    fix: js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+const fs = require('fs');
 
 const app = express();
 
@@ -17,11 +18,40 @@ app.get('/exchangeRate', (req, res) => {
     if (!error && response.statusCode === 200) {
       res.send(body);
     } else {
-      res.status(response.statusCode).end();
+      res.status(response.statusCode).send();
       console.log(`error = ${response.statusCode}`);
     }
   });
 });
+
+app.get('/getJson', (req, res) => {
+  let useDate;
+  for (let i = 1; i <= 30; i++) {
+    setTimeout(async () => {
+      useDate = `2023-11-${i}`;
+      console.log(`out ${useDate}`);
+      const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${useDate}&data=AP01`;
+      const options = {
+        url,
+      };
+      request.post(options, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const jsonString = JSON.stringify(body);
+          console.log(`inner ${useDate}`);
+          // 파일에 JSON 데이터 저장
+          fs.writeFile(`./data/${useDate}.json`, jsonString, (err) => {
+            if (err) throw err;
+            console.log('JSON 데이터가 파일에 저장되었습니다.');
+          });
+        } else {
+          res.status(response.statusCode).send();
+          console.log(`error = ${response.statusCode}`);
+        }
+      });
+    }, 1000);
+  }
+});
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running at ${port} ...`));
