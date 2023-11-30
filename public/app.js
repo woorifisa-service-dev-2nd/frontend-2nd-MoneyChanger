456037b58018b8971c204ddb4d0b2d77b3fc0d27
3-14\Admin <yjlee0235@naver.com>
2023-11-30 15:06:51 +0900

    Feat: Json 데이터 저장 기능 완성

--- public/app.js
+++ public/app.js
@@ -1,13 +1,9 @@
-function getToday() {
-  let useDate;
-  const today = new Date();
-  const yesterday = new Date(today.setDate(today.getDate() - 1));
-  const hour = today.getHours();
+function getQueryDate() {
+  const useDate = new Date();
+  const hour = useDate.getHours();
   // 오전 11시 이전이면 전 날짜로 가져오기
   if (hour < 11) {
-    useDate = yesterday;
-  } else {
-    useDate = today;
+    useDate.setDate(useDate.getDate() - 1);
   }
 
   const year = useDate.getFullYear(); // year
@@ -17,9 +13,30 @@ function getToday() {
   return `${year}-${month}-${date}`;
 }
 
+const makeExchangeRateContainer = (ex) => `<div class='container exchange-name'>${ex.cur_nm} (${ex.cur_unit})</div>
+  <div class='container exchange-receive'>전신환(송금) 받으실때 ${ex.ttb}</div>
+  <div class='container exchange-send'>전신환(송금) 보낼때 ${ex.tts}</div>
+  <div class='container exchange-trading-base-rate'>매매 기준율 ${ex.deal_bas_r}</div>`;
+
+function getExchangeRate(func, date = getQueryDate()) {
+  const exchangeRateRequest = new XMLHttpRequest();
+  exchangeRateRequest.onreadystatechange = () => {
+    // 데이터를 다 받았고, 응답코드 200(성공)을 받았는지 체크
+    if (exchangeRateRequest.readyState === 4 && exchangeRateRequest.status === 200) {
+      // 응답받은 데이터 체크
+      const body = JSON.parse(exchangeRateRequest.responseText);
+      const filteredBody = body.filter((currency) => currency.cur_unit !== 'KRW');
+      func(filteredBody);
+    }
+  };
+  exchangeRateRequest.open('GET', `/exchangeRate?date=${date}`, true);
+  exchangeRateRequest.send();
+}
+
 function newExchangeBox(exchangeRateJson) {
   const mainContainer = document.getElementsByClassName('main')[0];
   let containerIdx = -1;
+
   exchangeRateJson.forEach((ex, idx) => {
     if (idx % 5 === 0) {
       const newContainer = document.createElement('div');
@@ -27,15 +44,12 @@ function newExchangeBox(exchangeRateJson) {
       mainContainer.appendChild(newContainer);
       containerIdx += 1;
     }
+
     if (ex.cur_unit !== 'KRW') {
       const exchangeRateContainer = document.getElementsByClassName('exchange-rate')[containerIdx];
       const newContainer = document.createElement('div');
       newContainer.setAttribute('class', 'container flex-direction-column exchange-rate-box boreder-solid');
-      newContainer.innerHTML = `<div class='container'>${ex.cur_nm}(${ex.cur_unit})</div>
-    <div class='container'>전신환(송금) 받으실때${ex.ttb}</div>
-    <div class='container'>전신환(송금) 보낼때${ex.tts}</div>
-    <div class='container'>매매 기준율 ${ex.deal_bas_r}</div>
-    `;
+      newContainer.innerHTML = makeExchangeRateContainer(ex);
       exchangeRateContainer.appendChild(newContainer);
     }
   });
@@ -44,21 +58,4 @@ function newExchangeBox(exchangeRateJson) {
   mainContainer.appendChild(chartContainer);
 }
 
-function getExchangeRate(date = getToday(), func = newExchangeBox) {
-  const exchangeRateRequest = new XMLHttpRequest();
-  exchangeRateRequest.onreadystatechange = () => {
-    // 데이터를 다 받았고, 응답코드 200(성공)을 받았는지 체크
-    if (exchangeRateRequest.readyState === 4 && exchangeRateRequest.status === 200) {
-      // 응답받은 데이터 체크
-      const body = JSON.parse(exchangeRateRequest.responseText);
-      const filteredBody = body.filter((currency) => currency.cur_unit !== 'KRW');
-      console.log(body);
-      console.log(filteredBody);
-      func(filteredBody);
-    }
-  };
-  exchangeRateRequest.open('GET', `/exchangeRate?date=${date}`, true);
-  exchangeRateRequest.send();
-}
-
-getExchangeRate();
+window.onload = () => getExchangeRate(newExchangeBox);
--- server.js
+++ server.js
@@ -8,50 +8,74 @@ app.use(express.static('public'));
 app.use(express.json());
 
 const AUTH_KEY = 'kliY8rxDpuxOUUhVm05znmH2ts6FVf2c';
+
+const dateStringFrom = (dateObject) => {
+  const year = dateObject.getFullYear(); // year
+  const month = dateObject.getMonth() + 1; // month
+  const date = dateObject.getDate(); // date
+
+  return `${year}-${month}-${date}`;
+};
+
 app.get('/exchangeRate', (req, res) => {
-  const { date: useDate } = req.query;
-  const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${useDate}&data=AP01`;
+  const { date } = req.query;
+  const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${date}&data=AP01`;
+
   const options = {
     url,
   };
+
   request.post(options, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       res.send(body);
     } else {
-      res.status(response.statusCode).send();
-      console.log(`error = ${response.statusCode}`);
+      try {
+        res.status(response.statusCode).send();
+      } catch (err) {
+        console.error(err);
+      }
     }
   });
 });
 
 app.get('/getJson', (req, res) => {
-  let useDate;
-  for (let i = 1; i <= 30; i++) {
-    setTimeout(async () => {
-      useDate = `2023-11-${i}`;
-      console.log(`out ${useDate}`);
-      const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${useDate}&data=AP01`;
-      const options = {
-        url,
-      };
-      request.post(options, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const jsonString = JSON.stringify(body);
-          console.log(`inner ${useDate}`);
-          // 파일에 JSON 데이터 저장
-          fs.writeFile(`./data/${useDate}.json`, jsonString, (err) => {
-            if (err) throw err;
-            console.log('JSON 데이터가 파일에 저장되었습니다.');
-          });
-        } else {
-          res.status(response.statusCode).send();
-          console.log(`error = ${response.statusCode}`);
-        }
-      });
-    }, 1000);
+  // 어제부터 30일 전까지의 Data 생성
+  for (let i = 1; i <= 30; i += 1) {
+    const today = new Date();
+    today.setDate(today.getDate() - i);
+    const useDate = dateStringFrom(today);
+
+    const jsonFilePath = `./data/${useDate}.json`;
+    // 해당 날짜의 JSON 파일이 생성되어있지 않는 경우만 실행
+    if (!fs.existsSync(jsonFilePath)) {
+      setTimeout(async () => {
+        const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${useDate}&data=AP01`;
+        const options = {
+          url,
+        };
+        request.post(options, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const jsonString = JSON.stringify(body);
+
+            // 파일에 JSON 데이터 저장
+            fs.writeFile(jsonFilePath, jsonString, (err) => {
+              if (err) throw err;
+              console.log(`${useDate}일자 환율 데이터가 ${useDate}.json 파일에 저장되었습니다.`);
+            });
+            res.send();
+          } else {
+            try {
+              res.status(response.statusCode).send();
+            } catch (err) {
+              console.error(err);
+            }
+          }
+        });
+      }, 40 * i);
+    }
+    res.send();
   }
 });
-
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running at ${port} ...`));
