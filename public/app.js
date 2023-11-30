75fa258b22c59023720fc2a3fbe09e0fbab25c87
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-11-30 12:16:53 +0900

    refactor: js
    리펙토링

--- public/app.js
+++ public/app.js
@@ -19,8 +19,7 @@ function getExchangeRate(date = getToday(), func) {
     // 데이터를 다 받았고, 응답코드 200(성공)을 받았는지 체크
     if (exchangeRateRequest.readyState === 4 && exchangeRateRequest.status === 200) {
       // 응답받은 데이터 체크
-      const result = JSON.parse(exchangeRateRequest.responseText);
-      const body = JSON.parse(result.body);
+      const body = JSON.parse(exchangeRateRequest.responseText);
       const filteredBody = body.filter(currency => currency.cur_unit !== 'KRW');
       console.log(body);
       console.log(filteredBody);
--- server.js
+++ server.js
@@ -11,26 +11,17 @@ app.get('/exchangeRate', (req, res) => {
   const date = req.query.date;
   const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${date}&data=AP01`;
   const options = {
-    url: url
+    url,
   };
-  request.post(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.send(response);
+  request.post(options, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      res.send(body);
     } else {
       res.status(response.statusCode).end();
       console.log('error = ' + response.statusCode);
     }
   });
 });
-
-app.get('/', (req, res) => {
-  // html 파일 경로 설정
-  const htmlPath = path.resolve(__dirname + './public/index.html');
-
-  // html 파일 보여주기
-  res.sendFile(htmlPath);
-})
-
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running at ${port} ...`));
