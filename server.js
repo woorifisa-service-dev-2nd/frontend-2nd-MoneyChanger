3f076a820512faf7ce7d0c79952e1bbe00ba569f
3-14\Admin <yjlee0235@naver.com>
2023-11-30 16:33:55 +0900

    Feat: 요청한 Currency Unit에 대한 30일간 결과를 반환해주는 기능 추가

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ const dateStringFrom = (dateObject) => {
   return `${year}-${month}-${date}`;
 };
 
+const jsonFilePathFrom = (date) => `./data/${date}.json`;
+
+const beforeDateStringFromToday = (beforeDays) => {
+  const today = new Date();
+  today.setDate(today.getDate() - beforeDays);
+  return dateStringFrom(today);
+};
+
 app.get('/exchangeRate', (req, res) => {
   const { date } = req.query;
   const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${date}&data=AP01`;
@@ -41,40 +49,55 @@ app.get('/exchangeRate', (req, res) => {
 app.get('/getJson', (req, res) => {
   // 어제부터 30일 전까지의 Data 생성
   for (let i = 1; i <= 30; i += 1) {
-    const today = new Date();
-    today.setDate(today.getDate() - i);
-    const useDate = dateStringFrom(today);
-
-    const jsonFilePath = `./data/${useDate}.json`;
+    const useDate = beforeDateStringFromToday(i);
+    const jsonFilePath = jsonFilePathFrom(useDate);
     // 해당 날짜의 JSON 파일이 생성되어있지 않는 경우만 실행
     if (!fs.existsSync(jsonFilePath)) {
-      setTimeout(async () => {
-        const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${useDate}&data=AP01`;
-        const options = {
-          url,
-        };
-        request.post(options, (error, response, body) => {
-          if (!error && response.statusCode === 200) {
-            const jsonString = JSON.stringify(body);
+      const url = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=${AUTH_KEY}&searchdate=${useDate}&data=AP01`;
+      const options = {
+        url,
+      };
+      request.post(options, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const jsonString = JSON.stringify(body);
 
-            // 파일에 JSON 데이터 저장
+          // 파일에 JSON 데이터 저장
+          try {
             fs.writeFile(jsonFilePath, jsonString, (err) => {
               if (err) throw err;
               console.log(`${useDate}일자 환율 데이터가 ${useDate}.json 파일에 저장되었습니다.`);
             });
-            res.send();
-          } else {
-            try {
-              res.status(response.statusCode).send();
-            } catch (err) {
-              console.error(err);
-            }
+          } catch (err) {
+            console.error(err);
           }
-        });
-      }, 40 * i);
+        } else {
+          console.error(error);
+        }
+      });
     }
-    res.send();
   }
+
+  const currencyUnit = req.query.currency;
+  const before30DaysData = [];
+  // 30일 이전부터 최신순으로 반복
+  for (let beforeDays = 30; beforeDays > 0; beforeDays -= 1) {
+    const useDate = beforeDateStringFromToday(beforeDays);
+
+    const data = fs.readFileSync(jsonFilePathFrom(useDate), 'utf-8');
+    const jsonData = JSON.parse(JSON.parse(data));
+    // 주말인날은 제외
+    if (jsonData.length === 0) {
+      continue;
+    }
+
+    const filteredJsonData = jsonData.filter((ex) => ex.cur_unit === currencyUnit)[0];
+    const currencyUnitData = { date: useDate, ...filteredJsonData };
+
+    before30DaysData.push(currencyUnitData);
+  }
+
+  // client한테 요청한 curreny unit에 대한 30일간의 데이터를 모아서 반환
+  res.send(JSON.stringify(before30DaysData));
 });
 
 const port = 3000;
