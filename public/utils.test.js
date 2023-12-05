e175cf512db8b4d4d2e04ce1e716dc3bf6bf8616
mnjh01 <119735814+mnjihwan@users.noreply.github.com>
2023-12-05 12:36:56 +0900

    Update utils.test.js

--- public/utils.test.js
+++ public/utils.test.js
@@ -8,13 +8,16 @@ describe('test 1', () => {
 
 		const year = today.getFullYear();
 		const month = today.getMonth() + 1;
+		const beforeMonth = today.getMonth();
 		const date = today.getDate();
 
 		const expected = `${year}-${month}-${date < 10 ? '0' + date : date}`;
+		const unexpected = `${year}-${beforeMonth}-${date < 10 ? '0' + date : date}`;
 
 		const result = getQueryDate();
 
 		expect(result).toBe(expected);
+		expect(result).not.toBe(unexpected);
 	});
 
 	test.todo('asdf', () => {
